9c6c6fd3619af68a4bdfb428c234534d499dd3d7
TomasCipriano <d47173704@alumnos.itr.edu.ar>
2024-10-28 22:21:30 -0300

    tablas de incripciones listas

--- app/routes/incription.py
+++ app/routes/incription.py
@@ -1,3 +1,5 @@
+import requests
+#pip install requests
 from flask import Blueprint, flash, redirect, render_template, request, url_for
 from app import db
 from app.models import Inscripcion
@@ -14,6 +16,7 @@ def inscripciones():
 
     return render_template('inscripciones/inscripciones.html', Table1_inf=inscripciones, Table2_inf = equipos)
 
+#Cargar equipo manualmente a tabla1
 @teams_bp.route('/add_team', methods=['GET', 'POST'])
 def add_team():
     if request.method == 'POST':
@@ -38,12 +41,14 @@ def add_team():
         return redirect(url_for('teams_bp.inscripciones'))
     return render_template('inscripciones/add-team.html')
 
+
+#Editar equipo de tabla1
 @teams_bp.route('/edit/<int:id>')
 def get_team(id):
     equipo = Inscripcion.query.get_or_404(id)
     return render_template('inscripciones/edit-team.html', team=equipo)
 
-@teams_bp.route('/update/<int:id>', methods=['POST'])
+@teams_bp.route('/update_team/<int:id>', methods=['POST'])
 def update_team(id):
     equipo = Inscripcion.query.get_or_404(id)
     equipo.Equipo = request.form['Equipo']
@@ -62,29 +67,42 @@ def update_team(id):
     db.session.commit()
     return redirect(url_for('teams_bp.inscripciones'))
 
-@teams_bp.route('/delete/<int:id>')
-def delete_team(id):
-    equipo = Inscripcion.query.get_or_404(id)
-    db.session.delete(equipo)
-    db.session.commit()
-    return redirect(url_for('teams_bp.inscripciones'))
-
 @teams_bp.route('/cargar/<int:id>')
 def confirm_team(id):
+    # Confirmar el equipo en la base de datos
     equipo = Inscripcion.query.get_or_404(id)
     print(f"Estado antes: {equipo.Estado}")
     equipo.Estado = True
     db.session.commit()
     print(f"Estado después: {equipo.Estado}")
+
+    # # Llamar al Apps Script para crear el documento
+    # try:
+    #     script_url = 'https://docs.google.com/spreadsheets/d/1iC2vXoUpnRGCYjwohHsRJh7CBxR7YuNGtotHupB4ZMc/edit?usp=sharing'
+    #     response = requests.get(script_url, params={'id': id})
+        
+    #     if response.status_code == 200:
+    #         doc_url = response.json().get('doc_url')
+    #         # Guardar la URL del documento en el campo QR
+    #         equipo.QR = doc_url
+    #         db.session.commit()
+    #         flash("Equipo confirmado y documento creado exitosamente.", "success")
+    #     else:
+    #         flash("Error al crear el documento en Google Docs.", "error")
+    # except Exception as e:
+    #     flash(f"Error al llamar al Apps Script: {str(e)}", "error")
+
     return redirect(url_for('teams_bp.inscripciones'))
 
-@teams_bp.route('/get_info/<int:id>')
+
+#Editar equipo de tabla2
+@teams_bp.route('/edit2/<int:id>')
 def get_team2(id):
     equipo = Inscripcion.query.get_or_404(id)
     return render_template('inscripciones/final-config.html', team=equipo)
 
-@teams_bp.route('/config/<int:id>', methods=['POST'])
-def config_team(id):
+@teams_bp.route('/update_team2/<int:id>', methods=['POST'])
+def update_team2(id):
     if request.method == 'POST':
         equipo = Inscripcion.query.get_or_404(id)
         equipo.Equipo = request.form['Equipo']
@@ -96,6 +114,7 @@ def config_team(id):
         equipo.Correo = request.form['Correo']
         equipo.Miembros = request.form['Miembros']
         equipo.Acompañantes = request.form['Acompañantes']
+        equipo.Grupo = request.form['Grupo']
         equipo.Vegetariano = request.form['Vegetariano']
         equipo.Celiaco = request.form['Celiaco']
         equipo.Diabetico = request.form['Diabetico']
@@ -104,8 +123,9 @@ def config_team(id):
         return redirect(url_for('teams_bp.inscripciones'))
     return render_template('inscripciones/final-config.html')
 
-@teams_bp.route('/delete_copa/<int:id>', methods=['POST'])
-def eliminar_team(id):
+
+@teams_bp.route('/delete/<int:id>')
+def delete_team(id):
     equipo = Inscripcion.query.get_or_404(id)
     db.session.delete(equipo)
     db.session.commit()
