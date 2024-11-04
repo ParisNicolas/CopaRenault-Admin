b7a651991a486d5951a9c558b0fd6d25d4e20950
Joaquin Arce <d47174994@alumnos.itr.edu.ar>
2024-11-04 20:43:46 -0300

    Me dejo de andar el venv, me quiero matar

--- app/routes/sports.py
+++ app/routes/sports.py
@@ -1,33 +1,30 @@
-from flask import Blueprint, flash, redirect, render_template, request, url_for
+from flask import Blueprint, render_template, request
 from app import db
-from app.models import Inscripcion
-from app.models import Partido
 from app.models import Equipo
 from collections import defaultdict
 
-#from app.models import Carrito, Producto, CarritoProducto, Transaccion
-#from app.forms import TarjetaForm
-
 sport_bp = Blueprint('sport_bp', __name__, template_folder='templates')
 
-@sport_bp.route('/partidos', defaults={'deporte': 'futbol', 'categoria': 'mayor'})
+@sport_bp.route('/partidos', defaults={'deporte': None, 'categoria': None})
 @sport_bp.route('/partidos/<deporte>/<categoria>')
 def partidos(deporte, categoria):
+    # Filtrar equipos según el deporte y la categoría
+    query = Equipo.query
+    if deporte:
+        query = query.filter_by(deporte=deporte)
+    if categoria:
+        query = query.filter_by(categoria=categoria)
 
-    equipos = Equipo.query.filter_by(deporte=deporte, categoria=categoria).order_by(Equipo.grupo).all()
+    equipos = query.order_by(Equipo.grupo).all()
 
-    #agrupar por grupo
+    # Agrupar equipos por grupo
     equipos_por_grupo = defaultdict(list)
-    for equipo in equipos: 
+    for equipo in equipos:
         equipos_por_grupo[equipo.grupo].append(equipo)
 
-    #acceder a los equipos por grupo
-    for grupo, equipos in equipos_por_grupo.items():
-        print(f'Grupo {grupo}:')
-        for equipo in equipos:
-            print(f'{equipo.nombre} - {equipo.colegio}')
+    return render_template('deportes/partidos.html', grupos=equipos_por_grupo)
 
-    return render_template('deportes/partidos.html', grupos= equipos_por_grupo)
+    
     """datos = []
     for equipo in equipos:
         datos.append({"nombre":equipo.deporte, "grupo":registro.grupo_equipo, "pts":registro.puntos_equipo})
